72fe6aba6665091cc091833e40f487a27050dcbc
Santiago <sanalba2222@gmail.com>
2022-04-05 20:35:07 -0300

    2 paginas terminadas

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper2 = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  slidesPerGroup: 5,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
